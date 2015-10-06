0cc893c8cd5a1f5b515d583c7d121e750ec63941
AlexeyShamanin <shamaninae@mail.ru>
2015-10-06 20:06:09 +0300

    add solution

--- roman-time.js
+++ roman-time.js
@@ -3,30 +3,27 @@ var minutes = process.argv[3];
 
 // Немного замечательного кода и магии
 // my solution
-function convertToRoman (number){
+function convertToRoman (number)
+{
 var numbersArray = [1, 4, 5, 9, 10, 40, 50];
-var romanNumerals = {
-            1 : 'I',
-    		4 : 'IV',
-    		5 : 'V',
-    		9 : 'IX',
-    		10 : 'X',
-    		40 : 'XL',
-    		50 : 'L'
-                     };
+var romanNumerals = { 1 : 'I',4 : 'IV',5 : 'V',	9 : 'IX',10 : 'X',40 : 'XL',50 : 'L'};
 var count = numbersArray.length-1;
 var strInRoman = '';
-if (number == 0){
-      return '--';
-	}
-while (number > 0) {
-    if (number >=numbersArray[count]) {
-			strInRoman += romanNumerals[numbersArray[count]];
-			number -= numbersArray[count];
+if (number == 0)
+     {
+       return '--';
+     }
+while (number > 0)
+    {
+      if (number >=numbersArray[count])
+           {			
+             strInRoman += romanNumerals[numbersArray[count]];
+             number -= numbersArray[count];
+           }
+     else 
+           {
+             count--;
+           }
     }
-    else {
-        count--;
-         }
-  }
-	return strInRoman;
+          return strInRoman;
 }
